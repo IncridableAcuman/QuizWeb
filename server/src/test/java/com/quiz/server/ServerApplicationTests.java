1d14480fd3c7bd1abcf4edef0837b0c22d458ee4
Izzatbek <abdusharipovizzat03@gmail.com>
2025-04-23 19:01:21 +0500

    web quiz

--- server/src/test/java/com/quiz/server/ServerApplicationTests.java
+++ server/src/test/java/com/quiz/server/ServerApplicationTests.java
@@ -1,10 +1,20 @@
 package com.quiz.server;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootTest
 class ServerApplicationTests {
+	@BeforeAll
+	static void loadEnv(){
+		Dotenv dotenv=Dotenv.load();
+		dotenv.entries().forEach(entry->{
+			System.setProperty(entry.getKey(),entry.getValue());
+		});
+	}
 
 	@Test
 	void contextLoads() {
